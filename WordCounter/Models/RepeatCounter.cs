a5f6df49a5862b6246826862f76d46a54ad430de
Kevin Garvey <kmgarvey279@gmail.com>
2019-04-26 10:11:38 -0700

    First test successfull

--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -5,6 +5,13 @@ namespace WordCounter.Models
 {
   public class RepeatCounter
   {
+    private string _word;
+    private string _sentence;
 
+    public RepeatCounter(string word, string sentence)
+    {
+      _word = word;
+      _sentence = sentence;
+    }
   }
 }
