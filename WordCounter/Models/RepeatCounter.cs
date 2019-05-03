bcfe657a3e19425814e0818c6b838681766ebb71
Kevin Garvey <kmgarvey279@gmail.com>
2019-05-03 12:46:55 -0700

    Add stylesheet and Layout

--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -33,58 +33,6 @@ namespace WordCounter.Models
       return matches;
     }
 
-    //   foreach (char letter in _word)
-    //   {
-    //     if (!Char.IsLetter(letter))
-    //     {
-    //       return 0;
-    //     }
-    //   }
-    //   string wordToCheck = _word.ToUpper();
-    //   string sentenceToCheck = _sentence.ToUpper();
-    //   if(wordToCheck.Length > sentenceToCheck.Length)
-    //   {
-    //     return 0;
-    //   }
-    //   else if(wordToCheck.Length == sentenceToCheck.Length)
-    //   {
-    //     if(wordToCheck == sentenceToCheck)
-    //     {
-    //       return 1;
-    //     }
-    //     else
-    //     {
-    //       return 0;
-    //     }
-    //   }
-    //   else
-    //   {
-    //     for(int i = 0; i < sentenceToCheck.Length; i++)
-    //     {
-    //       if (wordToCheck[0] == sentenceToCheck[i])
-    //       {
-    //         if(i == 0)
-    //         {
-    //           string substringToCheck = sentenceToCheck.Substring(i, wordToCheck.Length);
-    //           if(substringToCheck == wordToCheck && !Char.IsLetter(sentenceToCheck[i+substringToCheck.Length]))
-    //           {
-    //             matches++;
-    //           }
-    //         }
-    //         else if (!Char.IsLetter(sentenceToCheck[i-1]))
-    //         {
-    //           string substringToCheck = sentenceToCheck.Substring(i, wordToCheck.Length);
-    //           if(substringToCheck == wordToCheck && !Char.IsLetter(sentenceToCheck[i+substringToCheck.Length]))
-    //           {
-    //             matches++;
-    //           }
-    //         }
-    //       }
-    //     }
-    //     return matches;
-    //   }
-    // }
-
     public string GetWord()
     {
       return _word;
@@ -103,15 +51,5 @@ namespace WordCounter.Models
     {
       _instances.Clear();
     }
-    //
-    // public static int GetId()
-    // {
-    //   return _id;
-    // }
-    //
-    // public static RepeatCounter Find(int searchId)
-    // {
-    //   return _instances[searchId-1];
-    // }
   }
 }
