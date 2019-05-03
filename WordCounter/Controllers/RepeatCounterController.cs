dc22674280fb677712f36ef22e6625ec3532a074
Kevin Garvey <kmgarvey279@gmail.com>
2019-05-03 09:54:32 -0700

    Add 'New' view/route

--- WordCounter/Controllers/RepeatCounterController.cs
+++ WordCounter/Controllers/RepeatCounterController.cs
@@ -1,9 +1,16 @@
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using WordCounter.Models;
-//
-// namespace WordCounter.Controllers
-// {
-//   public class
-// }
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
+
+namespace WordCounter.Controllers
+{
+  public class RepeatCounterController : Controller
+  {
+    [HttpGet("/repeatcounters/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+  }
+}
