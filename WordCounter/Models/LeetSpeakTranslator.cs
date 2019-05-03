81dd7c878b60053372ec1cbd7e0c1a2909cff65e
Kevin Garvey <kmgarvey279@gmail.com>
2019-05-03 16:56:07 -0700

    comment out leetspeak for the time being

--- WordCounter/Models/LeetSpeakTranslator.cs
+++ WordCounter/Models/LeetSpeakTranslator.cs
@@ -1,54 +1,58 @@
-using System;
-using System.Collections.Generic;
-
-namespace WordCounter.Models;
-{
-  public class LeetSpeakTranslator
-  {
-    private string _userInput;
-    private string _output;
-
-    public LeetSpeakTranslator(userInput)
-    {
-      _userInput = userInput;
-    }
-    public string GetInput()
-    {
-      return _userInput;
-    }
-    public string GetOutput()
-    {
-      return string _output;
-    }
-
-    public string Translate(string userInput)
-    {
-      char[] myArr = userInput.ToCharArray();
-      for(int i = 0; i < myArr.Length; i++)
-      {
-        if(myArr[i]== 'e' || myArr[i]== 'E')
-        {
-          myArr[i]= '3';
-        }
-        else if(myArr[i] =='o' || myArr[i] =='O')
-        {
-          myArr[i] = '0';
-        }
-        else if(myArr[i] == 'I')
-        {
-          myArr[i] = '1';
-        }
-        else if(myArr[i] == 't' || myArr[i] == 'T')
-        {
-          myArr[i] = '7';
-        }
-        else if (myArr[i] == 's' && i != 0 || myArr[i] == 'S' && i != 0 )
-        // else if(myArr[i] == 's' && myArr[i] >= 'A' && myArr[i] <= 'Z' && myArr[i] >= 'a' && myArr[i] <= 'z')
-        {
-          myArr[i] = 'Z';
-        }
-      }
-      _output = string.Join("", myArr);
-    }
-  }
-}
+// using System;
+// using System.Collections.Generic;
+//
+// namespace WordCounter.Models
+// {
+//   public class LeetSpeakTranslator
+//   {
+//     private string _userInput;
+//     private string _output;
+//
+//     public LeetSpeakTranslator(string userInput)
+//     {
+//       _userInput = userInput;
+//     }
+//     public string GetInput()
+//     {
+//       return _userInput;
+//     }
+//     public string GetOutput()
+//     {
+//       return _output;
+//     }
+//
+//     public string Translate()
+//     {
+//       char[] myArr = _userInput.ToCharArray();
+//       for(int i = 0; i < myArr.Length; i++)
+//       {
+//         if(myArr[i]== 'e' || myArr[i]== 'E')
+//         {
+//           myArr[i]= '3';
+//         }
+//         else if(myArr[i] =='o' || myArr[i] =='O')
+//         {
+//           myArr[i] = '0';
+//         }
+//         else if(myArr[i] == 'I')
+//         {
+//           myArr[i] = '1';
+//         }
+//         else if(myArr[i] == 't' || myArr[i] == 'T')
+//         {
+//           myArr[i] = '7';
+//         }
+//         else if (myArr[i] == 's' && i != 0 || myArr[i] == 'S' && i != 0 )
+//         // else if(myArr[i] == 's' && myArr[i] >= 'A' && myArr[i] <= 'Z' && myArr[i] >= 'a' && myArr[i] <= 'z')
+//         {
+//           myArr[i] = 'Z';
+//         }
+//         else
+//         {
+//           break;
+//         }
+//       }
+//       _output = string.Join("", myArr);
+//     }
+//   }
+// }
